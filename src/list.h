de1ccc356103556b6813462621a74d56101817c3
XDevil <andy890306@gmail.com>
2021-04-13 20:15:28 +0800

    Remove unnecessary struct in list.h

--- src/list.h
+++ src/list.h
@@ -3,23 +3,19 @@
 
 #include <stddef.h>
 
-struct list_head {
+typedef struct list_head {
     struct list_head *prev, *next;
-};
+} list_head;
 
-typedef struct list_head list_head;
-
-#define INIT_LIST_HEAD(ptr)           \
-    do {                              \
-        struct list_head *_ptr = ptr; \
-        (_ptr)->next = (_ptr);        \
-        (_ptr->prev) = (_ptr);        \
+#define INIT_LIST_HEAD(ptr)    \
+    do {                       \
+        list_head *_ptr = ptr; \
+        (_ptr)->next = (_ptr); \
+        (_ptr->prev) = (_ptr); \
     } while (0)
 
 /* Insert a new entry to two consecutive entries */
-static inline void __list_add(struct list_head *_new,
-                              struct list_head *prev,
-                              struct list_head *next)
+static inline void __list_add(list_head *_new, list_head *prev, list_head *next)
 {
     _new->next = next;
     next->prev = _new;
@@ -27,29 +23,29 @@ static inline void __list_add(struct list_head *_new,
     _new->prev = prev;
 }
 
-static inline void list_add(struct list_head *_new, struct list_head *head)
+static inline void list_add(list_head *_new, list_head *head)
 {
     __list_add(_new, head, head->next);
 }
 
-static inline void list_add_tail(struct list_head *_new, struct list_head *head)
+static inline void list_add_tail(list_head *_new, list_head *head)
 {
     __list_add(_new, head->prev, head);
 }
 
 /* Delete an entry to two consecutive entries */
-static inline void __list_del(struct list_head *prev, struct list_head *next)
+static inline void __list_del(list_head *prev, list_head *next)
 {
     prev->next = next;
     next->prev = prev;
 }
 
-static inline void list_del(struct list_head *entry)
+static inline void list_del(list_head *entry)
 {
     __list_del(entry->prev, entry->next);
 }
 
-static inline int list_empty(struct list_head *head)
+static inline int list_empty(list_head *head)
 {
     return (head->next == head) && (head->prev == head);
 }
